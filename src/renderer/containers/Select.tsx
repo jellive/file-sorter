7f2ffa248a026d4d3326f0d32c45af357e4f1496
Jellive <mystral7@naver.com>
2020-01-05 03:08:29 +0900

    add function 'get directory absolute path'.

--- src/renderer/containers/Select.tsx
+++ src/renderer/containers/Select.tsx
@@ -2,9 +2,27 @@ import * as React from 'react';
 
 const select = () => {
     const [dir, setDir] = React.useState('');
+
+    const changeFolder = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFolderNode: HTMLInputElement | null = document.getElementById(
+            'selectedfile'
+        ) as HTMLInputElement;
+        let path;
+        if (selectedFolderNode) {
+            path = selectedFolderNode.files[0].path;
+        }
+        alert(path);
+    };
+
     return (
         <>
-            <input type="file" webkitdirectory="" directory="" />
+            <input
+                id="selectedfile"
+                type="file"
+                webkitdirectory=""
+                directory=""
+                onChange={changeFolder}
+            />
         </>
     );
 };
