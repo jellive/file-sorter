789b905e9eb4476974300ad6e0b7a64ced1e4d69
Jellive <mystral7@naver.com>
2020-01-05 02:37:30 +0900

    remove counter and update library.

--- src/renderer/components/Application.tsx
+++ src/renderer/components/Application.tsx
@@ -5,8 +5,10 @@ import CounterContainer from '../containers/CounterContainer';
 
 const Application = () => (
     <div>
-        Hello World from Electron!
-        <CounterContainer />
+        <p>Hello!</p>
+        <br />
+        {/* <CounterContainer /> */}
+        <p>정렬할 디렉토리를 선택해주세요.</p>
     </div>
 );
 
