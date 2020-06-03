45259c0a2e6a115bd73473da9733533481fdc68b
Jellive <mystral7@naver.com>
2020-06-03 22:30:18 +0900

    change checkbox event.

--- src/renderer/components/FolderList.tsx
+++ src/renderer/components/FolderList.tsx
@@ -16,7 +16,7 @@ const FolderList = ({ folder, selectedFolder, addFolder, delFolder }: FolderProp
     const fileList = readDir(folder);
     const dirList = fileList.filter(file => fs.lstatSync(`${folder}/${file}`).isDirectory());
 
-    const dirClicked = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const dirClicked = (event: React.ChangeEvent<HTMLInputElement>, checked: boolean) => {
         console.log(
             event.currentTarget.value,
             selectedFolder,
@@ -27,29 +27,47 @@ const FolderList = ({ folder, selectedFolder, addFolder, delFolder }: FolderProp
             : delFolder(event.currentTarget.value);
     };
 
+    const allClicked = () => {
+        dirList.forEach(dir => addFolder(dir));
+    };
+
+    const rmvClicked = () => {
+        dirList.forEach(dir => delFolder(dir));
+    };
+
     return (
-        <Table>
-            <TableHead>
-                <TableRow>
-                    <TableCell>체크</TableCell>
-                    <TableCell>디렉토리 이름</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {dirList.map((file, index) => (
-                    <TableRow key={file}>
-                        <TableCell>
-                            <Checkbox onClick={dirClicked} value={file} />
-                        </TableCell>
-                        <TableCell>
-                            <p key={file + index}>{file}</p>
-                        </TableCell>
+        <>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>체크</TableCell>
+                        <TableCell>디렉토리 이름</TableCell>
                     </TableRow>
-                ))}
-            </TableBody>
-            <button type="button">모두 선택</button>
-            <button type="button">모두 해제</button>
-        </Table>
+                </TableHead>
+                <TableBody>
+                    {dirList.map((dir, index) => (
+                        <TableRow key={index}>
+                            <TableCell>
+                                <Checkbox
+                                    onChange={dirClicked}
+                                    value={dir}
+                                    checked={selectedFolder.indexOf(dir) > -1}
+                                />
+                            </TableCell>
+                            <TableCell>
+                                <p key={dir + index}>{dir}</p>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            <button type="button" onClick={allClicked}>
+                모두 선택
+            </button>
+            <button type="button" onClick={rmvClicked}>
+                모두 해제
+            </button>
+        </>
     );
 };
 
